5fd8c95a273e985a1bce2c1444e86725e5baf75f
DBWtv <dima.bwc@gmail.com>
2023-01-30 10:04:37 +0600

    ready to work

--- database/database.py
+++ database/database.py
@@ -1,4 +1,6 @@
-user_dict_template = {'page': 1,
-                      'bookmarks': set()}
+# Создаем шаблон заполнения словаря с пользователями
+user_dict_template: dict = {'page': 1,
+                            'bookmarks': set()}
 
-user_db = {}
+# Инициализируем "базу данных"
+users_db: dict = {}
